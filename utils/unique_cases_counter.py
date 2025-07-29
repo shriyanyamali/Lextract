d7e8027853827c0c5dbccce901bffdabcabbd5d6
ShriyanYamali <srujanshriyan@gmail.com>
2025-07-29 19:34:33 -0400

    renamed directory utils

---
+++ utils/unique_cases_counter.py
@@ -0,0 +1,36 @@
+# =============================================================================
+#
+# market-def-scraper - Extracts market definitions from European Commission's decision PDFs
+#
+# Copyright (C) 2025 Shriyan Yamali
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Contact: yamalishriyan@gmail.com
+#
+# =============================================================================
+
+import json
+input_file = r'' # Path to the JSON file containing case data
+
+def count_unique_cases(json_path):
+    with open(json_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    unique_cases = { item.get('case_number') for item in data if 'case_number' in item }
+
+    print(f"Found {len(unique_cases)} unique case numbers.")
+
+if __name__ == "__main__":
+    count_unique_cases(input_file)
